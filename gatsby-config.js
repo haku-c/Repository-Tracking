fbfc5757cf46d9ca4cc16f5cd62f391873214373
Huy Nguyen <kokoanimal@gmail.com>
2023-01-12 18:12:08 -0800

    Use gatsby-source-graphql plugin to query github graphql api

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,19 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require('dotenv').config()
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
+  plugins: [{
+    resolve: "gatsby-source-graphql",
+    options: {
+      typeName: "GitHub",
+      fieldName: "github",
+      url: "https://api.github.com/graphql",
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+      fetch,
+    },
+  }],
 }
